bbfb8b7d72086170c1db8bd0bcd399d2b78966b9
burnerwgu <dsavko@wgu.edu>
2025-10-06 12:48:04 -0400

    Step 2: Data Cleaning - implemented cleaning

--- main.py
+++ main.py
@@ -51,10 +51,19 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+                #Take the raw data and clean it and upload it
+            _ = mlflow.run(
+            os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+            entry_point="main",
+                parameters={
+                    "input_artifact": "sample.csv:latest",
+                    "output_artifact": "clean_sample.csv",
+                    "output_type": "cleaned_data",
+                    "output_description": "Cleaned data after basic processing",
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"],
+                },
+            )
 
         if "data_check" in active_steps:
             ##################
