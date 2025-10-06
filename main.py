bd30c2123bf586823b48c5ee013a5c0ff8478e7b
burnerwgu <dsavko@wgu.edu>
2025-10-06 13:37:18 -0400

    Step 3: Data Testing - implemented tests

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-                #Take the raw data and clean it and upload it
+            #Take the raw data and clean it and upload it
             _ = mlflow.run(
             os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
             entry_point="main",
@@ -66,10 +66,18 @@ def go(config: DictConfig):
             )
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            #Test data and ranges
+            _ = mlflow.run(
+            os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
+            entry_point="main",
+                parameters={
+                    "csv": "clean_sample.csv:latest",
+                    "ref": "clean_sample.csv:reference",
+                    "kl_threshold": config["data_check"]["kl_threshold"],
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"],
+                },
+            )
 
         if "data_split" in active_steps:
             ##################
